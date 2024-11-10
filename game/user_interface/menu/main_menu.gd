ad460f986988666c21feff94a74f9cffcdfee687
Def3P <122175474+Def3p@users.noreply.github.com>
2024-11-11 00:23:02 +0400

    Added Main Tree and edit script MainMenu

--- game/user_interface/menu/main_menu.gd
+++ game/user_interface/menu/main_menu.gd
@@ -1,11 +1,16 @@
+class_name MainMenu
 extends Node
+
+@export_category("Trees")
+@export var play_tree: PackedScene
+@export var settings_tree: PackedScene
 
 @onready var buttons = $Menu/VBoxButtons
 
-func _on_play_pressed() -> void: pass
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/levels/test/test_level.tscn")
 
 func _on_settings_pressed() -> void:
-	buttons.visible = buttons.visible
-	
+	pass
 
-#func _on_exit_pressed() -> void: get_tree().quit()
+func _on_exit_pressed() -> void: get_tree().quit()
