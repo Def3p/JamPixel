f381e24e32262a5efe458e025aaeddb8d920e64a
EGOPOS <egopos@yahoo.com>
2024-11-20 21:10:34 +0300

    settings with audio buses, resolution changing to main menu

--- game/user_interface/menu/main_menu.gd
+++ game/user_interface/menu/main_menu.gd
@@ -4,14 +4,19 @@ extends Node
 @export var play_tree: PackedScene
 @export var settings_tree: PackedScene
 
+@onready var settings_menu: MarginContainer = %SettingsMenu
 
 @onready var exit_confirm: MarginContainer = %ExitConfirm
 @onready var yes_exit_button: Button = %YesExitConfirmButton
 @onready var no_exit_button: Button = %NoExitConfirmButton
 
+@onready var play: TextureButton = %Play
+@onready var settings: TextureButton = %Settings
+
 
 func _ready():
 	exit_confirm.hide()
+	settings_menu.hide()
 	
 	yes_exit_button.pressed.connect(func():
 		get_tree().quit()
@@ -20,6 +25,9 @@ func _ready():
 	no_exit_button.pressed.connect(func():
 		exit_confirm.hide()
 	)
+	
+	play.pressed.connect(_on_play_pressed)
+	settings.pressed.connect(_on_settings_pressed)
 
 
 func _input(event: InputEvent) -> void:
@@ -31,4 +39,4 @@ func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/test/test_level.tscn")
 
 func _on_settings_pressed() -> void:
-	pass
+	settings_menu.visible = not settings_menu.visible
