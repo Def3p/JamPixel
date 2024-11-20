cb9835f9966e34bfa60fa7021a7a2c51228d07db
EGOPOS <egopos@yahoo.com>
2024-11-20 18:17:32 +0300

    exit to main menu

--- game/user_interface/menu/main_menu.gd
+++ game/user_interface/menu/main_menu.gd
@@ -4,6 +4,29 @@ extends Node
 @export var play_tree: PackedScene
 @export var settings_tree: PackedScene
 
+
+@onready var exit_confirm: MarginContainer = %ExitConfirm
+@onready var yes_exit_button: Button = %YesExitConfirmButton
+@onready var no_exit_button: Button = %NoExitConfirmButton
+
+
+func _ready():
+	exit_confirm.hide()
+	
+	yes_exit_button.pressed.connect(func():
+		get_tree().quit()
+	)
+	
+	no_exit_button.pressed.connect(func():
+		exit_confirm.hide()
+	)
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		exit_confirm.visible = not exit_confirm.visible
+
+
 func _on_play_pressed() -> void: 
 	get_tree().change_scene_to_file("res://scenes/levels/test/test_level.tscn")
 
